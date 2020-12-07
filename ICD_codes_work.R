1bbf0e068597c451522f90c07da14df92b1f1216
Luca Stefanucci <41801262+stefanucci-luca@users.noreply.github.com>
2020-12-07 10:29:18 +0000

    changed the way the code scrape the df. it looks just in 41270 now.

--- ICD_codes_work.R
+++ ICD_codes_work.R
@@ -21,8 +21,9 @@ message("The subset has ", dim(ukbdf200k)[1], " rows and ", dim(ukbdf200k)[2], "
 # Extract columns with ICD information for icd9, icd10:
 for (icd_version in 9:10){
 	ukbdf_icd = ukbdf %>%  
-		    dplyr::select(matches(paste("^diagnoses.*icd", icd_version, sep = ""))) # extract the icd columns
+	            dplyr::select(matches(paste("^diagnoses.*icd", icd_version, "_f41270*", sep = ""))) # extract the icd columns
 			# NOTE: this script consider only the ICD codes used in diagnosis,
+	    # either as first or secondary diagnosis (see fields 41270, 41202 and 41204 on UKBB research website)
 			# there are also other ICD codes used to describe cause of death, type of cancer, date of disease onset (which is very interesting)
 	message("there are ", dim(ukbdf_icd)[2], " ICD columns version ", icd_version, " in the datasheet") # print the number of columns with ICD values
 	# message("First ten entries are:\n", head(colnames(ukbdf_icd)))
