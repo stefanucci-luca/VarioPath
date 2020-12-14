2d855507f33d982ff2a89e741e168cc87ce0df61
Luca Stefanucci <41801262+stefanucci-luca@users.noreply.github.com>
2020-12-14 19:14:46 +0000

    last changes - need to fix the resource busy message

--- sanity_check.py
+++ sanity_check.py
@@ -6,15 +6,15 @@ from time import sleep
 import pysam
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import re
 import seaborn as sns
 
 # write in the path to the VCF or VCF like files to read
-vcf_to_read=['/home/ls760/hgmd_pro_2019.4_hg38.vcf',
-              '/home/ls760/clinvar_20200615.vcf'] 
+vcf_to_read=['/home/km369/.random_stuff/2019-4/hgmd_pro_2019.4_hg38.vcf',
+              '/home/km369/project-cbrc/VarioPath/clinvar_20200615.vcf'] 
 # write in the path to the file(s) that resemble a bed file but can't be parsed from any reader
-bed_to_read=['/home/ls760/curatedNBR_CSVD-TG-PAH-EAHAD-GRID-GPS_P-LP-VUS_GRCh38_v2_AssemConvert_sorted.bed']
+bed_to_read=['/home/km369/project-cbrc/VarioPath/curatedNBR_CSVD-TG-PAH-EAHAD-GRID-GPS_P-LP-VUS_GRCh38_v2_AssemConvert_sorted.bed']
 
 
 # Creates the list wjere I want to store the infromation from all the VCFs
@@ -65,9 +65,10 @@ with tempfile.TemporaryDirectory() as tmpdirname: # Create a tmp dir that store 
         strings = [list(rec.info)[i], rec.info[list(rec.info)[i]]] # it works on a single record. It creates a list of INFO_ID and INFO value.
         info.append('='.join(map(str, strings)))                   # concatenate values in a string e.g 'RS"=rs123 
       INFO.append(','.join(map(str, info)))                        # concatenate the string in an INFO list for all teh entries 
-  shutil.rmtree(dir, ignore_errors=False)
   os.chdir(actual_sir)    
+  shutil.rmtree(dir, ignore_errors=False)
 
+  
 # It is not possible to parse this file with existing parser. I created this simple one that extract the info I want.
 # Everything else goes in the INFO column
 for file in bed_to_read:
@@ -112,10 +113,62 @@ n_variatn_df = pd.read_csv("~/Desktop/VarioPath/number_variants_in_MDT_genes_cle
 n_variatn_df.head
 n_variatn_df.astype({'n_karyn_list': 'int32', 'n_variant_in_UKB': 'int32' })
 
-sns.relplot(x='n_karyn_list', y='n_variant_in_UKB', data = n_variatn_df, )
+var_ori=pd.read_csv("~/Desktop/VarioPath/TO_REMOVE", delimiter=',', skiprows=0) 
+var_ori.columns = ('gene', 'n_variant_unfiltered')
+
+n_variatn_df.gene=n_variatn_df.gene.astype(str)
+var_ori.gene=var_ori.gene.astype(str)
+df4=n_variatn_df.merge(var_ori, how='left')
+
+
+# created groups with genes fro every MDT
+blee_coag=["F10","F11","F12","F13A1","F13B",
+           "F2","F5","F7","F8","F9","FGA","FGB",
+           "FGG","GGCX","KNG1","LMAN1","MCFD2",
+           "SERPINE1","SERPINF2","VKORC1","VWF"]
+thrombosis=["ADAMTS13","HRG","PIGA","PLG","PROC",
+		          "PROS1","SERPINC1","SERPIND1","THBD"]
+platelet=["ABCC4","ABCG5","ABCG8","ACTB","ACTN1","ANKRD26",
+          "ANO6","AP3B1","AP3D1","ARPC1B","BLOC1S3","BLOC1S6",
+          "CDC42","CYCS","DIAPH1","DTNBP1","ETV6","FERMT3","FLI1",
+          "FLNA","FYB1","GATA1","GFI1B","GNE","GP1BA","GP1BB","GP6",
+          "GP9","HOXA11","HPS1","HPS3","HPS4","HPS5","HPS6","IKZF5",
+          "ITGA2B","ITGB3","KDSR","LYST","MECOM","MPIG6B","MPL","MYH9",
+          "NBEA","NBEAL2","P2RY12","PLA2G4A","PLAU","RASGRP2","RBM8A",
+          "RNU4ATAC","RUNX1","SLFN14","SRC","STIM1","STXBP2","TBXA2R",
+          "TBXAS1","THPO","TUBB1","VIPAS39","VPS33B","WAS"]
+hered_sfero = ["ANK1","EPB41","EPB42","SLC4A1","SPTA1","SPTB"]
+
+mdt_list=['blee_coag', 'thrombosis', 'platelet', 'hered_sfero']
+
+df4['MDT']='MDT_value'
+for i in mdt_list:
+  for j in range(0,len(df4['gene'])):
+    if df4.gene[j] in eval(i):
+      df4.MDT[j]=str(i)
+
+# function to plot the labels
+def label_point(x, y, val, ax):
+    a = pd.concat({'x': x, 'y': y, 'val': val}, axis=1)
+    for i, point in a.iterrows():
+      if len(val) < 10:
+        ax.text(point['x']+.02, point['y'], str(point['val']))
+      else:
+        if point.x > np.quantile(x,0.5) or point.y > np.quantile(y,0.5):
+          ax.text(point['x']+.02, point['y'], str(point['val']))
+
+sns.set_style("whitegrid")  #set polt style
+for i,grp in df4.groupby("MDT"):
+    plot=sns.lmplot(x="n_karyn_list", y="n_variant_in_UKB",  hue="gene", data=grp)
+    plot.set_titles(i)
+    label_point(grp.n_karyn_list, grp.n_variant_in_UKB, grp.gene, plt.gca())
 plt.show()
 
-# list pre pathogenicity filter
 
-sns.relplot(x='n_karyn_list', y='n_variant_in_UKB', data = df2 )
+sns.relplot(x='n_karyn_list', y='n_variant_in_UKB', hue='gene', col='MDT', data = df4)
+label_point(df4.n_karyn_list, df4.n_variant_in_UKB, df4.gene, plt.gca())
+plt.show()
+sns.regplot(x='n_variant_unfiltered', y='n_karyn_list', hue='gene', data = df4 )
+plt.show()
+sns.regplot(x='n_variant_unfiltered', y='n_variant_in_UKB', hue='gene', data = df4 )
 plt.show()
